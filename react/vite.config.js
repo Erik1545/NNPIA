0e6141d44ae710945caa77dbfee379130c0a91d0
erikz <st72713@upce.cz>
2024-06-16 15:08:47 +0200

    confuguration

--- react/vite.config.js
+++ react/vite.config.js
@@ -5,7 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
-    host: '0.0.0.0'
-  }
+    host: '0.0.0.0',
+    watch: {
+      usePolling: true,
+    },
+  },
 })
--- src/main/java/app/eshop/controller/ProductController.java
+++ src/main/java/app/eshop/controller/ProductController.java
@@ -8,12 +8,13 @@ import app.eshop.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+
+@RestController
+//@Controller
 public class ProductController {
 
     @Autowired
@@ -71,5 +72,15 @@ public class ProductController {
     public String displayDetails(@PathVariable(required = false) Long id, Model model){
         model.addAttribute("product", productRepository.findById(id).get());
         return "detail";
+    }
+
+    @GetMapping("/tryjson")
+    public List<String> allProducts(){
+        return productRepository.findAll().stream().map(Product::getProductName).toList();
+    }
+
+    @PostMapping("/tryjson")
+    public Product createProduct(@RequestBody Product product){
+        return productRepository.save(product);
     }
 }
